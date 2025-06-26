b998523e62968138cb6cb611243758e815b76f64
hz <beijingren@vip.qq.com>
2025-06-26 14:04:05 +0800

    更新依赖库版本

--- src/main/kotlin/com/zmkn/jackson/module/bson/BsonJacksonModule.kt
+++ src/main/kotlin/com/zmkn/jackson/module/bson/BsonJacksonModule.kt
@@ -1,8 +1,8 @@
 package com.zmkn.jackson.module.bson
 
 import com.fasterxml.jackson.databind.module.SimpleModule
-import com.zmkn.jackson.module.bson.serializers.ObjectIdSerializer
 import com.zmkn.jackson.module.bson.serializers.ObjectIdDeserializer
+import com.zmkn.jackson.module.bson.serializers.ObjectIdSerializer
 import org.bson.types.ObjectId
 
 object BsonJacksonModule {
